5ae6ba96e4b0971d99b28b4c055fc4d1c0774e0b
emaestro <f@emaestro.net>
2015-09-23 20:09:33 -0500

    Generamos Datos JSON

--- acercade.php
+++ acercade.php
@@ -1,8 +1,25 @@
+<?php
+	$Usuarios = array();
+	$usuario['paterno'] = 'GARCIA';
+	$usuario['materno'] = 'PEREZ';
+	$usuario['nombres'] = 'ALAN';
+	$usuario['usuario'] = 'alan';
+	$usuario['clave'] = 'garcia';
+	$Usuarios[] = $usuario;
+	$usuario['paterno'] = 'FLORES';
+	$usuario['materno'] = 'LOPEZ';
+	$usuario['nombres'] = 'LUIS';
+	$usuario['usuario'] = 'luis';
+	$usuario['clave'] = 'lopez';
+	$Usuarios[] = $usuario;
+?>
+
 <div class="row">
 	<div class="col-md-8">
-		<div class="alert alert-danger">
-			<h1>Acerca de</h1>
-		</div>
+		<?php
+
+		echo json_encode($Usuarios);
+		?>
 	</div>
 	<div class="col-md-4">
 
